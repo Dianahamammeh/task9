4b401c485246dff7d7b2dc0b3fc5529ed36af904
Dianahamammeh.com <daianahamammeh@gmail.com>
2024-05-09 14:31:12 +0300

    my project

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Otp\UserRegistrationOtp;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Route;
+
+use SadiqSalau\LaravelOtp\Facades\Otp;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthOtpController;
+use Illuminate\Support\Facades\Notification;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+route::get('/home',[AdminController::class,'index']);
+
+Route::controller(AuthOtpController::class)->group(function(){
+    Route::get('/otp/login', 'login')->name('otp.login');
+    Route::post('/otp/generate', 'generate')->name('otp.generate');
+    Route::get('/otp/verification/{user_id}', 'verification')->name('otp.verification');
+    Route::post('/otp/login', 'loginWithOtp')->name('otp.getlogin');
+});
